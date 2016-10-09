dae874e20d6011a06f4d0496ba8f9acbce1a4139
Yuki Morohoshi <hoshi.sanou@gmail.com>
2016-10-09 14:57:30 +0900

    move Board/Panel inside Rotzle namespace

--- app/rotzle/panel.rb
+++ app/rotzle/panel.rb
@@ -1,60 +1,62 @@
-class Panel < Sprite
-  FALL_SPEED = 10
+module Rotzle
+  class Panel < Sprite
+    FALL_SPEED = 10
 
-  attr_reader :cell
+    attr_reader :cell
 
-  def initialize
-    super(0, 0, self.class.const_get(:IMAGE))
-  end
-
-  def cell=(c)
-    @cell = c
-    self.x = 20 * c.x
-    self.y = 20 * c.y
-    @next_y = self.y
-  end
-
-  def fall_to(c)
-    @cell.panel = nil
-    c.instance_variable_set(:@panel, self)
-    @cell = c
-    @next_y = 20 * c.y
-  end
-
-  def linked_vanish!
-    @linked_vanish = true
-  end
-
-  def update
-    if self.y != @next_y
-      self.y += FALL_SPEED
-      @cell.board.falling!
-      return
+    def initialize
+      super(0, 0, self.class.const_get(:IMAGE))
     end
-    if @linked_vanish
-      self.alpha -= 15
-      if self.alpha > 0
-        @cell.board.vanishing!
-      else
-        @linked_vanish = false
-        @cell.panel = nil
+
+    def cell=(c)
+      @cell = c
+      self.x = 20 * c.x
+      self.y = 20 * c.y
+      @next_y = self.y
+    end
+
+    def fall_to(c)
+      @cell.panel = nil
+      c.instance_variable_set(:@panel, self)
+      @cell = c
+      @next_y = 20 * c.y
+    end
+
+    def linked_vanish!
+      @linked_vanish = true
+    end
+
+    def update
+      if self.y != @next_y
+        self.y += FALL_SPEED
+        @cell.board.falling!
+        return
+      end
+      if @linked_vanish
+        self.alpha -= 15
+        if self.alpha > 0
+          @cell.board.vanishing!
+        else
+          @linked_vanish = false
+          @cell.panel = nil
+        end
       end
     end
   end
-end
 
-class RedPanel < Panel
-  IMAGE = Image.new(20, 20, [230, 40, 30])
-end
+  class RedPanel < Panel
+    IMAGE = Image.new(20, 20, [230, 40, 30])
+  end
 
-class GreenPanel < Panel
-  IMAGE = Image.new(20, 20, [30, 200, 80])
-end
+  class GreenPanel < Panel
+    IMAGE = Image.new(20, 20, [30, 200, 80])
+  end
 
-class BluePanel < Panel
-  IMAGE = Image.new(20, 20, [30, 100, 230])
-end
+  class BluePanel < Panel
+    IMAGE = Image.new(20, 20, [30, 100, 230])
+  end
 
-class YellowPanel < Panel
-  IMAGE = Image.new(20, 20, [255, 200, 0])
+  class YellowPanel < Panel
+    IMAGE = Image.new(20, 20, [255, 200, 0])
+  end
 end
